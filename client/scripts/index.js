74986b970893c11a449a705b214f515260db54fb
0Thomas1 <0Thomas1@cs.sfu.ca>
2025-06-20 11:11:00 -0700

    first commit

---
+++ client/scripts/index.js
@@ -0,0 +1,144 @@
+(async () => {
+  function getHashParams() {
+    const hash = window.location.hash.substring(1);
+    return hash.split("&").reduce((res, item) => {
+      const [key, value] = item.split("=");
+      res[key] = decodeURIComponent(value);
+      return res;
+    }, {});
+  }
+
+  function displayPlaylists(playlists) {
+    if (playlists && playlists.items) {
+      playlists.items.forEach((playlist) => {
+        const playlistItemframe = document.createElement("div");
+        playlistItemframe.classList.add("col-3");
+        const playlistItem = document.createElement("div");
+        playlistItem.classList.add("card", "m-2");
+        playlistItem.innerHTML = `<strong>${playlist.name}</strong> - ${
+          playlist.description || "No description"
+        }
+        <a href="${
+          playlist.external_urls.spotify
+        }" target="_blank">View Playlist</a>
+        <button class="btn btn-primary m-2 edit-playlist" id="${
+          playlist.id
+        }">Edit Playlist</button>`;
+        playlistDiv.appendChild(playlistItemframe);
+        playlistItemframe.appendChild(playlistItem);
+      });
+    } else {
+      playlistDiv.innerHTML = "No playlists found.";
+    }
+  }
+
+  async function displaySongs(playlistId) {
+    playlistDiv.style.display = "none"; // Clear previous playlists
+
+    let playlist = await fetch(
+      "/fetchSongs?access_token=" + access_token + "&playlistId=" + playlistId
+    );
+    playlist = await playlist.json();
+    console.log("Fetched Playlist:", playlist);
+    let songs = playlist.items;
+
+    songs.forEach((song) => {
+      const songItem = document.createElement("div");
+      songItem.classList.add("card", "m-2", "width-50");
+      songItem.textContent = `${song.track.name} by ${song.track.artists
+        .map((artist) => artist.name)
+        .join(", ")}`;
+
+      const btnsDiv = document.createElement("div");
+      btnsDiv.classList.add("d-flex", "justify-content-between", "w-50");
+      const removeBtn = document.createElement("button");
+      removeBtn.classList.add("btn", "btn-danger");
+      removeBtn.textContent = "Remove";
+      removeBtn.onclick = async () => {
+        await fetch(
+          "/removeSong?access_token=" +
+            access_token +
+            "&songId=" +
+            song.track.uri +
+            "&playlistId=" +
+            playlistId,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        songItem.remove(); // Remove the song item from the UI
+      };
+      btnsDiv.appendChild(removeBtn);
+
+      const playBtn = document.createElement("button");
+      playBtn.classList.add("btn", "btn-success");
+      playBtn.textContent = "Play";
+      playBtn.onclick = async () => {
+        await fetch(
+          "/playSong?access_token=" +
+            access_token +
+            "&songId=" +
+            song.track.uri,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      };
+
+      btnsDiv.appendChild(playBtn);
+
+      const nextBtn = document.createElement("button");
+      nextBtn.classList.add("btn", "btn-secondary");
+      nextBtn.textContent = "Next";
+      btnsDiv.appendChild(nextBtn);
+
+      songItem.appendChild(btnsDiv);
+      songsDiv.appendChild(songItem);
+    });
+    songsDiv.style.display = "block"; // Show songs after fetching
+  }
+
+  const params = getHashParams();
+  let access_token = params.access_token;
+  let refresh_token = params.refresh_token;
+  let error = params.error;
+  let userDiv = document.getElementById("user-profile");
+  const playlistDiv = document.getElementById("playlists");
+  const songsDiv = document.getElementById("songs");
+  if (error) {
+    alert("error authing user");
+  } else {
+    if (access_token !== null) {
+      let userInfo = await fetch("/fetchUser?access_token=" + access_token);
+      userInfo = await userInfo.json();
+      userDiv.innerHTML = userInfo.display_name || "No user info available";
+      let userPlaylists = await fetch(
+        "/fetchUserPlaylists?access_token=" + access_token
+      );
+      userPlaylists = await userPlaylists.json();
+      displayPlaylists(userPlaylists);
+    }
+  }
+
+  // You can now use access_token and refresh_token as needed
+  console.log("Access Token:", access_token);
+  console.log("Refresh Token:", refresh_token);
+  console.log("Error: ", error);
+
+  document.addEventListener("click", async (event) => {
+    if (event.target.classList.contains("edit-playlist")) {
+      songsDiv.innerHTML = ""; // Clear previous songs
+
+      const playlistId = event.target.id;
+      console.log("Selected Playlist ID:", playlistId);
+      await displaySongs(playlistId);
+      playlistDiv.classList.add("d-none"); // Hide playlists
+    }
+  });
+})();
---
+++ server.js
@@ -0,0 +1,231 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const redirect_uri = "http://localhost:" + PORT + "/callback";
+const stateKey = "spotify_auth_state";
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const crypto = require("crypto");
+const querystring = require("querystring");
+const cookieParser = require("cookie-parser");
+const request = require("request");
+
+const generateRandomString = (length) => {
+  return crypto.randomBytes(60).toString("hex").slice(0, length);
+};
+
+app
+  .use(express.static(__dirname + "/client", { index: "index.html" }))
+  .use(cookieParser())
+  .use(express.json());
+
+app.get("/fetchUser", async (req, res) => {
+  let query = req.query;
+  console.log("Query Parameters:", query);
+  let token = query.access_token || req.headers.authorization?.split(" ")[1];
+  console.log("Access Token:", token);
+  let result = await fetch("https://api.spotify.com/v1/me", {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  result = await result.json();
+  console.log("User Info:", result);
+  res.send(result);
+});
+
+app.put("/playSong", async (req, res) => {
+  let query = req.query;
+  console.log("Query Parameters:", query);
+  let token = query.access_token || req.headers.authorization?.split(" ")[1];
+  let songId = query.songId;
+  console.log("Access Token:", token);
+  console.log("Song ID:", songId);
+
+  let result = await fetch(`https://api.spotify.com/v1/me/player/play`, {
+    method: "PUT",
+    headers: { Authorization: `Bearer ${token}` },
+    body: JSON.stringify({ uris: [songId] }),
+  });
+
+  if (result.error) {
+    res.send(result.error);
+  } else {
+    res.send({ message: "Song is now playing" });
+  }
+});
+app.get("/fetchSongs", async (req, res) => {
+  let query = req.query;
+  console.log("Query Parameters:", query);
+  let token = query.access_token;
+  let playlistId = query.playlistId;
+  console.log("Access Token:", token);
+  let result = await fetch(
+    `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+    {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+  result = await result.json();
+  res.send(result);
+});
+
+app.delete("/removeSong", async (req, res) => {
+  let query = req.query;
+  console.log("Query Parameters:", query);
+  let token = query.access_token || req.headers.authorization?.split(" ")[1];
+  let songId = query.songId;
+  let playlistId = query.playlistId;
+  console.log("Access Token:", token);
+  console.log("Song ID:", songId);
+  console.log("Playlist ID:", playlistId);
+
+  let result = await fetch(
+    `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+    {
+      method: "DELETE",
+      headers: { Authorization: `Bearer ${token}` },
+      body: JSON.stringify({ tracks: [{ uri: songId }] }),
+    }
+  );
+
+  if (result.error) {
+    res.send(result.error);
+  } else {
+    res.send({ message: "Song removed from playlist" });
+  }
+});
+
+app.get("/fetchUserPlaylists", async (req, res) => {
+  let query = req.query;
+  console.log("Query Parameters:", query);
+  let token = query.access_token || req.headers.authorization?.split(" ")[1];
+  console.log("Access Token:", token);
+  let result = await fetch("https://api.spotify.com/v1/me/playlists", {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  result = await result.json();
+  console.log("User Playlists:", result);
+  res.send(result);
+});
+app.get("/login", (req, res) => {
+  let state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  let scope =
+    "user-read-private user-read-email user-modify-playback-state playlist-modify-private playlist-modify-public";
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+      })
+  );
+});
+app.get("/", (req, res) => {});
+
+app.get("/callback", function (req, res) {
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  let code = req.query.code || null;
+  let state = req.query.state || null;
+  let storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect(
+      "/#" +
+        querystring.stringify({
+          error: "state_mismatch",
+        })
+    );
+  } else {
+    res.clearCookie(stateKey);
+    let authOptions = {
+      url: "https://accounts.spotify.com/api/token",
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: "authorization_code",
+      },
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " +
+          new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+      },
+      json: true,
+    };
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        let access_token = body.access_token,
+          refresh_token = body.refresh_token;
+
+        let options = {
+          url: "https://api.spotify.com/v1/me",
+          headers: { Authorization: "Bearer " + access_token },
+          json: true,
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function (error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect(
+          "/#" +
+            querystring.stringify({
+              access_token: access_token,
+              refresh_token: refresh_token,
+            })
+        );
+      } else {
+        res.redirect(
+          "/#" +
+            querystring.stringify({
+              error: "invalid_token",
+            })
+        );
+      }
+    });
+  }
+});
+
+app.get("/refresh_token", function (req, res) {
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+    },
+    form: {
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
+    },
+    json: true,
+  };
+
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      let access_token = body.access_token,
+        refresh_token = body.refresh_token;
+      res.send({
+        access_token: access_token,
+        refresh_token: refresh_token,
+      });
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
